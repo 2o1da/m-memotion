f680a349d70c15402d56d52bd52d1caf7bca18b9
DaSol Kim <solda@khu.ac.kr>
2021-06-23 23:56:34 +0900

    Modify the files to clean up

--- client/src/index.js
+++ client/src/index.js
@@ -9,13 +9,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// let h2 = document.querySelector("h2");
-// let text = document.querySelector("h2").innerHTML;
-
 // h2.innerHTML = "";
 // for (let i = 0; i < text.length; ++i) {
 //   setTimeout(function () {
